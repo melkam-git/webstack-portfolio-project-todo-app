340a370d2a66cdd4269a53acc2d8ce51a9ab6b0f
melkam-git <melkamalemnew16@gmail.com>
2026-05-10 15:20:01 +0300

    update

--- backend/src/middleware/authorization.ts
+++ backend/src/middleware/authorization.ts
@@ -1,87 +1,36 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { ApiKey } from "../api.key/api.key.entity";
 import logger from "../common/logger";
-import { goodRequest, noGoodRequest } from "../common/response";
+import { unauthorized } from "../common/response";
 import { base64ToString } from "../common/string";
 import { Database } from "../database";
-import { User } from "./user.entity";
-import { UserSession, UserSessionStatus } from "./user.session.entity";
-import { v4 as uuid } from "uuid";
 
-const UserRepository = Database.getRepository(User);
-const UserSessionRepository = Database.getRepository(UserSession);
+const ApiKeyRepository = Database.getRepository(ApiKey)
 
-class UserController {
+export default async function (req: Request, res: Response, next: NextFunction) {
+	logger.debug('authorization')
+	const { authorization } = req.headers
 
-    async createNewUser(req: Request, res: Response) {
-        logger.debug("create new user");
+	const [bearer, string] = (authorization as string).split(' ')
+	if (!string) {
+		logger.debug('no auth string found')
+		return unauthorized(res)
+	}
 
-        const { fullname, username, password } = req.body;
+	const { "api-key": apiKey, "api-key-user": apiKeyUser, username, token } = JSON.parse(base64ToString(string))
+	if (!apiKey) {
+		logger.debug('no apiKey found')
+		return unauthorized(res)
+	}
 
-        if (!fullname || !username || !password)
-            return noGoodRequest(res, "missing fields");
+	const apiKeys = await ApiKeyRepository.findOneBy({ usedBy: apiKeyUser })
+	if (!apiKeys && !apiKey.compareTokenSync(apiKey)) {
+		logger.debug('no api key match found')
+		unauthorized(res)
+	}
 
-        const existing = await UserRepository.findOneBy({ username });
-        if (existing)
-            return noGoodRequest(res, "username already used");
 
-        const newUser = new User();
-        newUser.fullname = fullname;
-        newUser.username = username;
-
-        await newUser.setPassword(base64ToString(password));
-
-        await UserRepository.save(newUser);
-
-        goodRequest(res);
-    }
-
-    async login(req: Request, res: Response) {
-        logger.debug("login");
-
-        const { username, password } = req.body;
-
-        const loginUser = await UserRepository.findOneBy({ username });
-
-        if (
-            !loginUser ||
-            !(await loginUser.comparePassword(base64ToString(password)))
-        ) {
-            return noGoodRequest(res, "username/password incorrect");
-        }
-
-        const token = uuid();
-
-        const newSession = new UserSession();
-        newSession.user = loginUser;
-
-        await newSession.setToken(token);
-        await UserSessionRepository.save(newSession);
-
-        goodRequest(res, ["token", token]);
-    }
-
-    async resumeSession(req: Request, res: Response) {
-        logger.debug("resume session");
-        goodRequest(res);
-    }
-
-    async logout(req: Request, res: Response) {
-        logger.debug("logout");
-
-        const { userSession } = res.locals;
-
-        userSession.status = UserSessionStatus.Revoked;
-
-        await UserSessionRepository.save(userSession);
-
-        goodRequest(res);
-    }
-
-    userInfo(req: Request, res: Response) {
-        logger.debug("user info");
-
-        goodRequest(res, ["user", res.locals.user]);
-    }
+	res.locals.username = username
+	res.locals.token = token
+	next()
 }
-
-export default new UserController();
